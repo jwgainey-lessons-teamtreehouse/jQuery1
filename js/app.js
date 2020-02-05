0fa78394afd3abe87b45da4fd65709975d180e7b
Joel Gainey <jwgainey@hotmail.com>
2020-02-05 12:37:27 -0500

    .text() and .html()

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$('#flashMessage').hide().slideDown(1000).delay(3000).slideUp();
+$('#flashMessage')
+    .hide()
+    .slideDown(1000)
+    .delay(3000)
+    .slideUp();
+
+const title = "My First Blog Post";
+const content = "This is my first blog post!";
+
+$('#blogTitlePreview').text(title);
+$('#blogContentPreview').text(content);
