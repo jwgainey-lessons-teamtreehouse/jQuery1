a9bf50515c586cc599e478fd6fc8c23bdf16935c
Joel Gainey <jwgainey@hotmail.com>
2020-02-05 14:06:28 -0500

    val();

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
-$('#flashMessage')
-    .hide()
-    .slideDown(1000)
-    .delay(3000)
-    .slideUp();
+/* 
 
 const title = "My First Blog Post";
 const content = "This is my first blog post!";
 
-$('#blogTitlePreview').text(title);
-$('#blogContentPreview').text(content);
+*/
+
+$('#flashMessage').hide();
+
+$('#previewButton').click(function() {
+   const title = $('#blogTitleInput').val();
+   const content = $('#blogContentInput').val();
+
+   $('#blogTitlePreview').text(title);
+$('#blogContentPreview').text(content); 
+
+$('#flashMessage')
+    .slideDown(1000)
+    .delay(3000)
+    .slideUp();
+});
